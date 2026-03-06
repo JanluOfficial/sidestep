817459ac8fa2b656580a53adad17060d86021586
Janlu <realdjjanlu@gmail.com>
2026-03-06 09:45:04 +0100

    Reformatted menu.h

--- menu.h
+++ menu.h
@@ -7,18 +7,18 @@
 using namespace std;
 
 class Menu {
-    public:
-        string menu_title = "";
-        vector<string> items = {};
-        vector<string> option_text = {};
-        vector<function<void()>> callbacks = {};
-        Menu(string title);
-        int selected = 0;
+  public:
+    string menu_title = "";
+    vector<string> items = {};
+    vector<string> option_text = {};
+    vector<function<void()>> callbacks = {};
+    Menu(string title);
+    int selected = 0;
 
-        void addItem(string item, function<void()> callback = nullptr);
-        void update_option_text(int index, string text);
-        void incrementSelected();
-        void executeSelected();
+    void addItem(string item, function<void()> callback = nullptr);
+    void update_option_text(int index, string text);    
+    void incrementSelected();
+    void executeSelected();
 };
 
 void DrawMenu(Menu menu);

605994f441c4311f46eedc102e7ed0337e0601d3
Janlu <realdjjanlu@gmail.com>
2026-03-06 10:18:37 +0100

    Reformatted player.h

--- player.h
+++ player.h
@@ -1,10 +1,9 @@
 class Player {
-    public:
-        int x;
-        int score;
-
-        Player() {
-            x = 3; // Leftmost position = 0, Center position = 3, Rightmost position = 6
-            score = 0;
-        }
+  public:
+    int x;
+    int score;
+    Player() {
+      x = 3; // Leftmost position = 0, Center position = 3, Rightmost position = 6
+      score = 0;
+    }
 };

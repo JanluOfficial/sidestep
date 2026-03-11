0cb9e3f65b41d2d47e128874f7870be84ee8bb9b
Janlu <realdjjanlu@gmail.com>
2026-03-11 12:41:50 +0100

    Added SceneManager

---
+++ scene_manager.cpp
@@ -0,0 +1,18 @@
+#include "scene_manager.h"
+#include "base_scene.h"
+#include <memory>
+using namespace std;
+
+void SceneManager::ChangeScene(unique_ptr<BaseScene> newScene) {
+  if (currentScene) currentScene->Teardown();
+  currentScene = std::move(newScene);
+  currentScene->Setup();
+}
+
+void SceneManager::Update() {
+  if (currentScene) currentScene->Update();
+}
+
+void SceneManager::Draw() {
+  if (currentScene) currentScene->Draw();
+}
---
+++ scene_manager.h
@@ -0,0 +1,18 @@
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+
+#include <memory>
+
+class BaseScene;
+
+class SceneManager {
+private:
+  std::unique_ptr<BaseScene> currentScene;
+
+public:
+  void ChangeScene(std::unique_ptr<BaseScene> newScene);
+  void Update();
+  void Draw();
+}
+
+#endif // SCENE_MANAGER_H

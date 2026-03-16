23a04fc8800bd3bae37424934a8d8051f15b782f
Janlu <realdjjanlu@gmail.com>
2026-03-16 09:57:25 +0100

    Implemented LogoScene

---
+++ include/scenes/logo_scene.h
@@ -0,0 +1,23 @@
+#ifndef LOGO_SCENE_H
+#define LOGO_SCENE_H
+
+#include "base_scene.h"
+#include <raylib.h>
+
+class LogoScene : public BaseScene {
+private:
+  float timer = 0.0f;
+  int textureId = 0;
+  Texture2D logos[4];
+  Sound buildup;
+  Sound jingle;
+  bool jinglePlayed = false;
+
+public:
+  void Setup() override;
+  void Update(SceneManager *manager) override;
+  void Draw() override;
+  void Teardown() override;
+};
+
+#endif // LOGO_SCENE_H
---
+++ src/scenes/logo_scene.cpp
@@ -0,0 +1,45 @@
+#include "scene_manager.h"
+#include "scenes/logo_scene.h"
+#include <raylib.h>
+#include <cmath>
+
+void LogoScene::Setup() {
+  logos[0] = LoadTexture("resources/textures/jpix0.png");
+  logos[1] = LoadTexture("resources/textures/jpix1.png");
+  logos[2] = LoadTexture("resources/textures/jpix2.png");
+  logos[3] = LoadTexture("resources/textures/jpix3.png");
+
+  buildup = LoadSound("resources/sounds/janlu_jingle_buildup.ogg");
+  jingle = LoadSound("resources/sounds/janlu_jingle.ogg");
+
+  PlaySound(buildup);
+}
+
+void LogoScene::Update(SceneManager* manager) {
+  timer += GetFrameTime();
+  textureId = (int)fmin(floor(timer * 3), 3.0f);
+
+  if (timer > 1.5f && !jinglePlayed) {
+    PlaySound(jingle);
+    jinglePlayed = true;
+  }
+
+  /*if (timer > 3.0f && !IsSoundPlaying(jingle)) {
+    manager->ChangeScene(std::make_unique<MainMenuScene>());
+  }*/
+}
+
+void LogoScene::Draw() {
+  DrawTexture(
+    logos[textureId],
+    GetScreenWidth() / 2 - logos[textureId].width / 2,
+    GetScreenHeight() / 2 - logos[textureId].height / 2,
+    WHITE
+  );
+}
+
+void LogoScene::Teardown() {
+  for (int i = 0; i < 4; i++) UnloadTexture(logos[i]);
+  UnloadSound(buildup);
+  UnloadSound(jingle);
+}

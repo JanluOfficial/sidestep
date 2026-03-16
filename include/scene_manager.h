391f1107c450848ed824cbe42807d38babd4b05c
JanluOfficial <realdjjanlu@gmail.com>
2026-03-16 20:53:43 +0100

    Added color values into scene manager

--- include/scene_manager.h
+++ include/scene_manager.h
@@ -2,6 +2,7 @@
 #define SCENE_MANAGER_H
 
 #include <memory>
+#include <raylib.h>
 
 class BaseScene;
 
@@ -10,9 +11,18 @@ class SceneManager {
     std::unique_ptr<BaseScene> currentScene;
 
   public:
+    Color primary_color     = GREEN;
+    Color secondary_color   = LIME;
+    Color teritary_color    = DARKGREEN;
+    Color bg_color          = BLACK;
+
+    // Core
     void ChangeScene(std::unique_ptr<BaseScene> newScene);
     void Update();
     void Draw();
+
+    // Color customization
+    void SetColors(Color primary, Color secondary, Color teritary, Color bg);
 };
 
 #endif // SCENE_MANAGER_H
--- src/scene_manager.cpp
+++ src/scene_manager.cpp
@@ -1,8 +1,10 @@
 #include "scene_manager.h"
 #include "base_scene.h"
 #include <memory>
+#include <raylib.h>
 using namespace std;
 
+// Core
 void SceneManager::ChangeScene(unique_ptr<BaseScene> newScene) {
   if (currentScene) currentScene->Teardown();
   currentScene = std::move(newScene);
@@ -16,3 +18,11 @@ void SceneManager::Update() {
 void SceneManager::Draw() {
   if (currentScene) currentScene->Draw();
 };
+
+// Color customization
+void SceneManager::SetColors(Color primary, Color secondary, Color teritary, Color bg) {
+  primary_color     = primary;
+  secondary_color   = secondary;
+  teritary_color    = teritary;
+  bg_color          = bg;
+}

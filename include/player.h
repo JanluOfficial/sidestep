095134dd4ebe5aad06cb65542578ba662bbaaa28
Janlu <realdjjanlu@gmail.com>
2026-03-23 11:10:37 +0100

    Simplified Player class

--- include/player.h
+++ include/player.h
@@ -3,12 +3,8 @@
 
 class Player {
   public:
-    int x;
-    int score;
-    Player() {
-      x = 3; // Leftmost position = 0, Center position = 3, Rightmost position = 6
-      score = 0;
-    }
+    int x = 3;
+    int score = 0;
 };
 
 #endif // PLAYER_H

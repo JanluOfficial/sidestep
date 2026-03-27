69772ac189b8d8faf781ebb6d5b936bee11f0c77
Janlu <realdjjanlu@gmail.com>
2026-03-27 10:50:21 +0100

    Added penalty logic, pausing and input.h based input handling

--- src/scenes/game_scene.cpp
+++ src/scenes/game_scene.cpp
@@ -6,9 +6,13 @@
 #include <raylib.h>
 #include <string>
 
-void penalty(Player *player) {
+#define L_BUFFERABLE map.map[2] != 0 && tick_timer > 1 / game_speed * 0.7
+
+bool penalty(Player *player) {
+  if (player->rempen_cooldown > 0) return false;
   player->rempen -= 1;
   player->rempen_cooldown = 1.5;
+  return true;
 }
 
 void GameScene::Setup(SceneManager *manager) {
@@ -16,61 +20,72 @@ void GameScene::Setup(SceneManager *manager) {
   milestone10k = LoadSound("resources/sounds/milestone_tt.ogg");
   milestone100k = LoadSound("resources/sounds/milestone_ht.ogg");
   milestone1m = LoadSound("resources/sounds/milestone_m.ogg");
+
+  move = LoadSound("resources/sounds/menu_tick.ogg");
+  dmg = LoadSound("resources/sounds/fail.ogg");
 }
 
 void GameScene::Update(SceneManager *manager) {
-  if (IsKeyPressed(KEY_G)) {
-    if (player.x > 0 && map.map[1][player.x - 1] == 1) {
-      for (int ix = player.x - 1; ix >= 0; ix--) {
-        if (map.map[1][ix] == 0) break;
-        player.x = ix;
-        player.score += 10;
-        moved++;
+  if (!paused){
+    if (IsKeyPressed(KEY_G)) {
+      if (player.x > 0 && map.map[1][player.x - 1] != 0) {
+        for (int ix = player.x - 1; ix >= 0; ix--) {
+          if (map.map[1][ix] == 0) break;
+          player.x = ix;
+          player.score += 10;
+          moved++;
+        }
+      } else {
+        if (penalty(&player)) PlaySound(dmg);
       }
-    } else if (map.map[1][player.x + 1] == 0 && !(player.rempen_cooldown > 0)) {
-      penalty(&player);
     }
-  }
 
-  if (IsKeyPressed(KEY_H)) {
-    if (player.x < 6 && map.map[1][player.x + 1] == 1) {
-      for (int ix = player.x + 1; ix <= 6; ix++) {
-        if (map.map[1][ix] == 0) break;
-        player.x = ix;
-        player.score += 10;
-        moved--;
+    if (IsKeyPressed(KEY_H) || l_buffered && tick_timer < 0.1) {
+      if (player.x < 6 && map.map[1][player.x + 1] != 0) {
+        for (int ix = player.x + 1; ix <= 6; ix++) {
+          if (map.map[1][ix] == 0) break;
+          player.x = ix;
+          player.score += 10;
+          moved--;
+        }
+      } else {
+        if (penalty(&player)) PlaySound(dmg);
       }
-    } else if (map.map[1][player.x - 1] == 0 && !(player.rempen_cooldown > 0)) {
-      penalty(&player);
     }
+
+    if (map.map[1][player.x] == 0 || player.rempen == 0) {
+      manager->ChangeScene(std::make_unique<MainMenu>());
+      return;
+    }
+
+    if (tick_timer >= 1 / game_speed) {
+      map.generateNextSegment();
+      player.score += 5;
+      tick_timer = 0.0f;
+    }
+
+    if (old_score % 1000000 > player.score % 1000000) {
+      PlaySound(milestone1m);
+    } else if (old_score % 100000 > player.score % 100000) {
+      PlaySound(milestone100k);
+    } else if (old_score % 10000 > player.score % 10000) {
+      PlaySound(milestone10k);
+    } else if (old_score % 1000 > player.score % 1000) {
+      PlaySound(milestone1k);
+    }
+
+    float frame_time = GetFrameTime();
+    timer += frame_time;
+    tick_timer += GetFrameTime();
+    if (player.rempen_cooldown > 0) {
+      player.rempen_cooldown -= frame_time;
+    }
+    old_score = player.score;
   }
 
-  if (map.map[1][player.x] == 0 || player.rempen == 0) {
-    manager->ChangeScene(std::make_unique<MainMenu>());
-    return;
+  if (IsKeyPressed(KEY_SPACE)) {
+    paused = !paused;
   }
-
-  if (old_score % 1000000 > player.score % 1000000) {
-    PlaySound(milestone1m);
-  } else if (old_score % 100000 > player.score % 100000) {
-    PlaySound(milestone100k);
-  } else if (old_score % 10000 > player.score % 10000) {
-    PlaySound(milestone10k);
-  } else if (old_score % 1000 > player.score % 1000) {
-    PlaySound(milestone1k);
-  }
-
-  float frame_time = GetFrameTime();
-  timer += frame_time;
-  tick_timer += GetFrameTime();
-  if (player.rempen_cooldown > 0) {
-    player.rempen_cooldown -= frame_time;
-  }
-  if (tick_timer >= 1 / game_speed) {
-    map.generateNextSegment();
-    tick_timer = 0.0f;
-  }
-  old_score = player.score;
 
   primary = manager->primary_color;
   bg = manager->bg_color;
@@ -99,6 +114,15 @@ void GameScene::Draw() {
   for (int i = 0; i < player.rempen; i++) {
     DrawRectangle(20 + 60 * i, 100, 50, 20, primary);
   }
+
+  // Paused
+  if (paused) {
+    Color pause_bg = {0, 0, 0, 204};
+    DrawRectangle(0, 0, sw, sh, pause_bg);
+    DrawText("PAUSED", cx - 100, cy - 20, 40, primary);
+  }
+
+  //if (L_BUFFERABLE) DrawRectangle(20, 140, 50, 50, RED);
 }
 
 void GameScene::Teardown() {
@@ -106,4 +130,7 @@ void GameScene::Teardown() {
   UnloadSound(milestone10k);
   UnloadSound(milestone100k);
   UnloadSound(milestone1m);
+
+  UnloadSound(move);
+  UnloadSound(dmg);
 }

370386965123fa3acd69074d7ea3ba54eb8f7ff5
Janlu <realdjjanlu@gmail.com>
2026-04-13 22:01:06 +0200

    Started implementing settings_scene

--- include/scenes/settings_scene.h
+++ include/scenes/settings_scene.h
@@ -8,25 +8,37 @@
 #include <functional>
 
 struct MenuOption {
-    std::string label;
-    std::string secondary_label;
+  std::string label;
+  std::string secondary_label;
 
-    std::function<void()> action = nullptr;  // Action to perform when selected if not sub menu
-    std::vector<MenuOption> sub_options; // If vector not empty, open sub menu when selected
+  std::function<void()> action = nullptr;  // Action to perform when selected if not sub menu
+  std::vector<MenuOption> sub_options; // If vector not empty, open sub menu when selected
 
-    bool has_sub_menu() const { return !sub_options.empty(); }
+  MenuOption(std::string l, std::function<void()> a = nullptr, std::vector<MenuOption> sub = {})
+    : label(l), action(a), sub_options(sub) {}
+
+  bool has_sub_menu() const { return !sub_options.empty(); };
+  int sub_menu_size() const { return sub_options.size(); };
 };
 
 class SettingsScene : public BaseScene {
   private:
     float timer = 0.0f;
 
-    int sub_menu = 0;
     int selected = 0;
+
+    std::vector<MenuOption> menu_root;
+    std::vector<MenuOption>* current_menu = nullptr;
+    std::vector<std::vector<MenuOption>*> menu_stack;
+
+    int current_menu_size() const { return current_menu->size(); };
 
   public:
     Color primary;
     Color bg;
+
+    void NavigateForward();
+    void NavigateBack(SceneManager *man);
 
     void Setup(SceneManager *manager) override;
     void Update(SceneManager *manager) override;
--- src/scenes/settings_scene.cpp
+++ src/scenes/settings_scene.cpp
@@ -1,17 +1,61 @@
+#include "input.h"
 #include "scene_manager.h"
 #include "scenes/settings_scene.h"
+#include "scenes/main_menu.h"
 #include <raylib.h>
+#include <memory>
+
+void SettingsScene::NavigateForward() {
+  auto& item = (*current_menu)[selected];
+
+  if (item.has_sub_menu()) {
+    menu_stack.push_back(current_menu);
+    current_menu = &item.sub_options;
+    selected = 0;
+  } else if (item.action) {
+    item.action();
+  }
+}
+
+void SettingsScene::NavigateBack(SceneManager *man) {
+  if (!menu_stack.empty()) {
+    current_menu = menu_stack.back();
+    menu_stack.pop_back();
+    selected = 0;
+  } else {
+    man->ChangeScene(std::make_unique<MainMenu>());
+  }
+}
 
 void SettingsScene::Setup(SceneManager *manager) {
+  menu_root = {
+    {"Audio", nullptr, {
+      {"Music Volume", []() { /* Adjust Vol */ }},
+      {"SFX Volume",   []() { /* Adjust Vol */ }}
+    }},
+    {"Graphics", nullptr, {
+      {"Fullscreen",   []() { /* Toggle */ }}
+    }},
+    {"Back to Main", [manager]() { manager->ChangeScene(std::make_unique<MainMenu>()); }}
+  };
 
+  current_menu = &menu_root;
 }
 
 void SettingsScene::Update(SceneManager *manager) {
+  timer += GetFrameTime();
 
+  if (IsInputLeftPressed()) {
+    selected = (selected + 1) % current_menu_size();
+  }
+
+  if (IsInputRightPressed()) {
+    NavigateForward();
+  }
 }
 
 void SettingsScene::Draw() {
-
+  DrawRectangle(10 * selected, 10, 10, 10, RED);
 }
 
 void SettingsScene::Teardown() {

ca4efbe5a5c39c332be969984a256231ea8e6576
Janlu <realdjjanlu@gmail.com>
2026-07-15 09:17:56 +0200

    Added config.h

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Default resolution of the game window
+const int game_width_default = 1280; // Default: 1280
+const int game_height_default = 720; // Default: 720
+
+// Size of the audio stream buffer (default: 4096)
+const int audio_stream_buffer_size = 4096;
+
+#endif // CONFIG_H
